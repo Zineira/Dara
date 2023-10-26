d7f33fb98672154b7c8b8b619847b252e55ada55
Henrique Dias <henriquezineira@gmail.com>
2023-10-26 14:31:20 +0100

    instr done

--- Dara/board.js
+++ Dara/board.js
@@ -3,13 +3,12 @@ const colors = ['#123456']
 const SQUARES_NUM_1 = 25
 const SQUARES_NUM_2 = 30
 
-for(let i =0;i <SQUARES_NUM_2;i++){
+for(let i =0;i <SQUARES_NUM_1;i++){
     const square = document.createElement('div');
     square.classList.add('square')
 
     square.addEventListener('mouseover', () => setColor(square))
     square.addEventListener('mouseout', () => resetColor(square))
-
     board.append(square)
 }
 
@@ -29,15 +28,47 @@ function getRandomColor(){
     return colors[index]
 }
 
-document.getElementById('instrbutton').addEventListener('click', function() {
-    var box1 = document.querySelector('.rules');
-    var box2 = document.querySelector('#board')
-    
-    if (box1.style.display === 'none') {
-        box1.style.display = 'block';
-        box2.style.display = 'none';
-    } else {
+window.onload = () => {
+    const instructionsButton = document.getElementById('instrbutton');
+    const rankingButton = document.getElementById('rankbutton');
+    const box1 = document.querySelector('.rules');
+    const box2 = document.querySelector('.box-main');
+    const box3 = document.querySelector('.score');
+    const overlay = document.querySelector('.overlay');
+
+    instructionsButton.addEventListener('click', function() {
+        if (box1.style.display === 'none') {
+            box1.style.display = 'block';
+            box2.style.display = 'none';
+            box3.style.display = 'none';
+            overlay.style.display = 'block'; // Mostra a cobertura
+        } else {
+            box1.style.display = 'none';
+            box2.style.display = 'grid';
+            box3.style.display = 'none';
+            overlay.style.display = 'none'; // Oculta a cobertura
+        }
+    });
+
+    rankingButton.addEventListener('click', function() {
+        if (box3.style.display === 'none') {
+            box3.style.display = 'block';
+            box2.style.display = 'none';
+            box1.style.display = 'none';
+            overlay.style.display = 'block'; // Mostra a cobertura
+        } else {
+            box3.style.display = 'none';
+            box2.style.display = 'grid';
+            box1.style.display = 'none';
+            overlay.style.display = 'none'; // Oculta a cobertura
+        }
+    });
+
+    // Adicione um evento de clique na cobertura para fechar as instruções ou o ranking
+    overlay.addEventListener('click', function() {
         box1.style.display = 'none';
-        box2.style.display = 'block grid';
-    }
-});
+        box2.style.display = 'grid';
+        box3.style.display = 'none';
+        overlay.style.display = 'none';
+    });
+};
