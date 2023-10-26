bc935bbd35d91539bfc57d8b7ce9ba1d546ee43c
Henrique Dias <henriquezineira@gmail.com>
2023-10-26 15:48:36 +0100

    faltam fazer as peças movimentar

--- Dara/board.js
+++ Dara/board.js
@@ -1,31 +1,34 @@
-const board = document.querySelector('#board')
-const colors = ['#123456']
-const SQUARES_NUM_1 = 25
-const SQUARES_NUM_2 = 30
+const rightbox = document.querySelector('.rightbox');
+const pieces = document.querySelector('.pieces');
+const colors = ['#123456'];
 
-for(let i =0;i <SQUARES_NUM_1;i++){
-    const square = document.createElement('div');
-    square.classList.add('square')
-
-    square.addEventListener('mouseover', () => setColor(square))
-    square.addEventListener('mouseout', () => resetColor(square))
-    board.append(square)
+// Crie e posicione as peças na "rightbox"
+function createPiece(className) {
+    const piece = document.createElement("div");
+    piece.className = "game-piece " + className;
+    pieces.appendChild(piece);
 }
 
-function setColor(element){
-    const color = getRandomColor()
-    element.style.backgroundColor=color
-    element.style.boxShadow = '0 0 2px ${color},0 0 10px ${color}'
-
+// Lista de peças (12 brancas e 12 pretas)
+const piece_list = [];
+for (let i = 0; i < 12; i++) {
+    piece_list.push(createPiece("white"));
+    piece_list.push(createPiece("black"));
 }
 
-function resetColor(element){
-    element.style.backgroundColor= 'rgba(104, 160, 229, 0)'
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
-function getRandomColor(){
-    const index = Math.floor(Math.random() * colors.length)
-    return colors[index]
+function resetColor(element) {
+    element.style.backgroundColor = 'rgba(104, 160, 229, 0)';
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
 }
 
 window.onload = () => {
@@ -37,7 +40,7 @@ window.onload = () => {
     const overlay = document.querySelector('.overlay');
 
     instructionsButton.addEventListener('click', function() {
-        if (box1.style.display === 'none') {
+        if (box1.style.display != 'block') {
             box1.style.display = 'block';
             box2.style.display = 'none';
             box3.style.display = 'none';
@@ -51,7 +54,7 @@ window.onload = () => {
     });
 
     rankingButton.addEventListener('click', function() {
-        if (box3.style.display === 'none') {
+        if (box3.style.display != 'block') {
             box3.style.display = 'block';
             box2.style.display = 'none';
             box1.style.display = 'none';
