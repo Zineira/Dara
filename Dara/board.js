4274641486096d6cdd3e85b26d7f1265c582d9ba
Henrique Dias <henriquezineira@gmail.com>
2023-10-30 16:51:29 +0000

    backup

--- Dara/board.js
+++ Dara/board.js
@@ -1,6 +1,8 @@
 const rightbox = document.querySelector('.rightbox');
 const pieces = document.querySelector('.pieces');
 const colors = ['#123456'];
+let opponentsSelect; // Declaração da variável opponentsSelect
+let difficultySelect; // Declaração da variável difficultySelect
 
 
 function createBoard(element){
@@ -56,8 +58,6 @@ for (let i = 0; i < 12; i++) {
 for (let i = 0; i < 12; i++) {
     piece_list.push(createPiece("black"));
 }
-
-// Adicionar peças ao container
 piece_list.forEach(piece => pieces.appendChild(piece));
 
 function setColor(element) {
@@ -73,6 +73,45 @@ function getRandomColor() {
     const index = Math.floor(Math.random() * colors.length);
     return colors[index];
 }
+// funçoes dos botões
+
+function checkSelections() {
+    const boardSizeSelect = document.getElementById('board_size');
+    const opponentsSelect = document.getElementById('oponents');
+    const firstSecondSelect = document.getElementById('firstsecond');
+    const difficultySelect = document.getElementById('difficulty');
+    const playButton = document.getElementById('play');
+    
+    if (opponentsSelect.value === 'twoplayer') {
+        // Se o adversário for um jogador humano, oculte a seleção de dificuldade
+        difficultySelect.style.display = 'none';
+    } else {
+        // Se o adversário for um bot, exiba a seleção de dificuldade
+        difficultySelect.style.display = 'inline-block';
+    }
+
+    if (
+        boardSizeSelect.value === 'None' ||
+        opponentsSelect.value === 'None' || 
+        firstSecondSelect.value === 'None'
+    ) {
+        playButton.disabled = true;
+    } else {
+        playButton.disabled = false;
+    }
+    updateBotName();
+}
+function updateBotName() {
+    const opponentsValue = opponentsSelect.value;
+    const difficultyValue = difficultySelect.value;
+    const difficultyText = difficultyValue.charAt(0).toUpperCase() + difficultyValue.slice(1);  // Capitalize o primeiro caractere
+
+    if (opponentsValue === 'oneplayer') {
+        opponentName.textContent = "Bot " + difficultyText;
+    } else {
+        opponentName.textContent = "Player2";
+    }
+}
 
 window.onload = () => {
     const instructionsButton = document.getElementById('instrbutton');
@@ -84,6 +123,8 @@ window.onload = () => {
     const scorebox = document.querySelector('.score');
     const overlay = document.querySelector('.overlay');
 
+    
+    //Regras
     instructionsButton.addEventListener('click', function() {
         if (rulesbox.style.display != 'block') {
             rulesbox.style.display = 'block';
@@ -97,7 +138,7 @@ window.onload = () => {
             overlay.style.display = 'none'; // Oculta a cobertura
         }
     });
-
+    //pontuaçoes
     rankingButton.addEventListener('click', function() {
         if (scorebox.style.display != 'block') {
             scorebox.style.display = 'block';
@@ -111,36 +152,76 @@ window.onload = () => {
             overlay.style.display = 'none'; // Oculta a cobertura
         }
     });
-    playButton.addEventListener('click', function(){
-        console.log("Botão Jogar clicado");
-        if (mainbox.style.display !== 'none') {
-            if (board.style.display !== 'grid') {
-                board.style.display = 'grid';
-                if (!board.hasChildNodes()) {  // Verifica se o tabuleiro já foi criado
-                    createBoard(board);
-                }
-            } else {
-                board.style.display = 'none';
-            }
-        }
-        pieces.style.display= 'flex'; 
-        resetButton.style.display = 'block';
-    });
+    //jogar
+    const boardSizeSelect = document.getElementById('board_size');
+    opponentsSelect = document.getElementById('oponents');
+    const firstSecondSelect = document.getElementById('firstsecond');
+    difficultySelect = document.getElementById('difficulty');
 
+    boardSizeSelect.addEventListener('change', checkSelections);
+    opponentsSelect.addEventListener('change', checkSelections);
+    firstSecondSelect.addEventListener('change', checkSelections);
+    difficultySelect.addEventListener('change', checkSelections);
+
+    checkSelections();
+    
+    playButton.addEventListener('click', function(){
+        opponentsSelect = document.getElementById('oponents'); // Atualiza a variável opponentsSelect
+        checkSelections();
+        if (mainbox.style.display !== 'none') {
+            // Sempre exiba a board como uma grid quando o botão "Jogar" for clicado
+            board.style.display = 'grid';
+            // Sempre crie (ou recrie) o tabuleiro quando o botão "Jogar" for clicado
+            createBoard(board);
+            pieces.style.display= 'flex'; 
+            resetButton.style.display = 'block';
+        }
+        const playerBox = document.querySelector('.player-box');
+        const whitePlayer = document.querySelector('.white-player');
+        const blackPlayer = document.querySelector('.black-player');
+    
+        // Mostrar a caixa de informação do jogador
+        playerBox.style.display = 'flex';
+    
+        // Posicionar os elementos do jogador de acordo com a seleção
+        const firstSecondValue = firstSecondSelect.value;
+        if (firstSecondValue === 'white') {
+            whitePlayer.style.order = '1';  // A ordem de flex é 1 para o jogador branco
+            blackPlayer.style.order = '2';  // A ordem de flex é 2 para o jogador preto
+        } else if (firstSecondValue === 'black') {
+            whitePlayer.style.order = '2';  // A ordem de flex é 2 para o jogador branco
+            blackPlayer.style.order = '1';  // A ordem de flex é 1 para o jogador preto
+        }
+        boardSizeSelect.disabled = true;
+        opponentsSelect.disabled = true;
+        firstSecondSelect.disabled = true;
+        difficultySelect.disabled = true;
+    });
+    // reset
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', function(){
-    // Limpa o tabuleiro
-    board.innerHTML = '';
-    // Reseta as configurações
-    document.getElementById('board_size').value = 'None';
-    document.getElementById('oponents').value = 'None';
-    document.getElementById('firstsecond').value = 'None';
-    document.getElementById('difficulty').value = 'None';
-    // Oculta o botão reset
-    pieces.style.display= 'none'; 
-    resetButton.style.display = 'none';
-});
-    // Adicione um evento de clique na cobertura para fechar as instruções ou o ranking
+        board.innerHTML = '';
+        playButton.disabled = true;
+        document.getElementById('board_size').value = 'None';
+        document.getElementById('oponents').value = 'None';
+        document.getElementById('firstsecond').value = 'None';
+        document.getElementById('difficulty').value = 'None';
+        boardSizeSelect.disabled = false;
+        opponentsSelect.disabled = false;
+        firstSecondSelect.disabled = false;
+        difficultySelect.disabled = false;
+        pieces.style.display= 'none'; 
+        resetButton.style.display = 'none';
+    
+    });
+    const opponentDifficulty = document.getElementById('opponent-difficulty');
+
+    
+
+
+// Defina o valor inicial
+opponentsSelect.dispatchEvent(new Event('change'));
+    
     overlay.addEventListener('click', function() {
         rulesbox.style.display = 'none';
         mainbox.style.display = 'grid';
@@ -149,13 +230,14 @@ window.onload = () => {
     });
 };
 
+//overlays dos jogadores
 
 const playerInfo = document.getElementById('player-info');
-const playerName = document.getElementById('player-name');
+const playerName = document.querySelector('.white-player .player-name');
 const playerPiece = document.getElementById('player-piece');
 
 const opponentInfo = document.getElementById('opponent-info');
-const opponentName = document.getElementById('opponent-name');
+const opponentName = document.querySelector('.black-player .player-name');
 const opponentPiece = document.getElementById('opponent-piece');
 
 // Define as informações do jogador
@@ -163,5 +245,5 @@ playerName.textContent = "Player1";  // Substitua "Seu Nome" pelo nome do jogado
 playerPiece.textContent = "Peças Brancas";
 
 // Define as informações do oponente (pode ser outro jogador ou computador)
-opponentName.textContent = "Player2";  // Substitua "Oponente" pelo nome do oponente
+opponentName.textContent = "Bot";  // Substitua "Oponente" pelo nome do oponente
 opponentPiece.textContent = "Peças Pretas";
